fa33a8a77051d9b51785bb985a1a6df2f369454b
Greg Orlov <greg@avvo.com>
2015-08-04 18:01:36 -0700

    fixing error message in honeybadger notifier

--- lib/la_maquina/error_notifier/honeybadger_notifier.rb
+++ lib/la_maquina/error_notifier/honeybadger_notifier.rb
@@ -2,7 +2,7 @@ module LaMaquina
   module ErrorNotifier
     class HoneybadgerNotifier < LaMaquina::ErrorNotifier::Base
       def notify(error, details = {})
-        Honeybadger.notify( :error_class => "CacheMachineError: #{error.class.name}",
+        Honeybadger.notify( :error_class => "LaMaquinaError: #{error.class.name}",
                             :error_message => error.message,
                             :parameters => details
                           )
