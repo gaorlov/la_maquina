03b70ee4982cb825ea089138847da1301db00bc2
Greg Orlov <greg@avvo.com>
2015-08-05 11:10:43 -0700

    fixing honeybadger notifier

--- lib/la_maquina/error_notifier/honeybadger_notifier.rb
+++ lib/la_maquina/error_notifier/honeybadger_notifier.rb
@@ -1,7 +1,7 @@
 module LaMaquina
   module ErrorNotifier
     class HoneybadgerNotifier < LaMaquina::ErrorNotifier::Base
-      def notify(error, details = {})
+      def self.notify(error, details = {})
         Honeybadger.notify( :error_class => "LaMaquinaError: #{error.class.name}",
                             :error_message => error.message,
                             :parameters => details
--- lib/la_maquina/version.rb
+++ lib/la_maquina/version.rb
@@ -1,3 +1,3 @@
 module LaMaquina
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
